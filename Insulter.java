7e1bc621ad698d31ad12fcecc9f3c4afacd2aa36
J. Birkel <birkel_judson@salkeiz.k12.or.us>
2023-11-07 17:20:28 +0000

    Added 10 lines to each of the 3 word generating functions, plus the initial random_range() function.

---
+++ Insulter.java
@@ -0,0 +1,78 @@
+public class Insulter
+{
+    public static void main(String[] args)
+    {
+        String randWord1 = get1stWord();
+        String randWord2 = get2ndWord();
+        String randWord3 = get3rdWord();
+        System.out.println("Thou "+randWord1+" "+randWord2+" "+randWord3+"!");
+    }
+    public static int random_range(int min, int max)
+    {
+        int size = max - min + 1;
+        return (int)(Math.random() * size)+min;
+    }
+    public static String get1stWord()
+    {
+        int min = 0;
+        int max = 10;
+        int rand = random_range(min, max);
+        switch(rand)
+        {
+            case 0: return "artless";
+            case 1: return "bawdy";
+            case 2: return "beslubbering";
+            case 3: return "bootless";
+            case 4: return "churlish";
+            case 5: return "cockered";
+            case 6: return "clouted";
+            case 7: return "craven";
+            case 8: return "currish";
+            case 9: return "dankish";
+            case 10: return "dissembling";
+            default: return "Error!";
+        }
+    }
+    public static String get2ndWord()
+    {
+        int min = 0;
+        int max = 10;
+        int rand = random_range(min, max);
+        switch(rand)
+        {
+            case 0: return "base-court";
+            case 1: return "bat-forling";
+            case 2: return "beef-witted";
+            case 3: return "beetle-headed";
+            case 4: return "boil-brained";
+            case 5: return "clapper-clawed";
+            case 6: return "clay-brained";
+            case 7: return "common-kissing";
+            case 8: return "crook-pated";
+            case 9: return "dismal-dreaming";
+            case 10: return "dizzy-eyed";
+            default: return "Error!";
+        }
+    }
+    public static String get3rdWord()
+    {
+        int min = 0;
+        int max = 10;
+        int rand = random_range(min, max);
+        switch(rand)
+        {
+            case 0: return "apple-john";
+            case 1: return "baggage";
+            case 2: return "barnacle";
+            case 3: return "bladder";
+            case 4: return "boar-pig";
+            case 5: return "bugbear";
+            case 6: return "bum-bailey";
+            case 7: return "canket-blossom";
+            case 8: return "clack-dish";
+            case 9: return "clotpole";
+            case 10: return "coxcomb";
+            default: return "Error!";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Shakespearean Insult Generator
 
 This program generates 3 random numbers which access a single word from 3 different lists, which when combined creates a shakespearean Insult.
+
+There are several lists of Shakespearean Insults (there's even a flipbook out there! https://www.amazon.com/Shakespeare-Insult-Generator-Match-Insults/dp/1452127751), but Christian Heilmann's website (https://christianheilmann.com/2020/08/19/fun-project-shakespeare-insult-generator/) holds the same first list I was introduced to.
